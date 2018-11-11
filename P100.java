5293778263b61ff6bf1311ae0ed3eeff5bb67ae3
Elsa Sarrías <elsa.sar.ali@gmail.com>
2018-11-11 23:37:50 +0100

    submitted version 1

--- P100.java
+++ P100.java
@@ -1,9 +1,13 @@
+package AA; 
+
 import java.util.ArrayList;
 
 public class P100{
 
     public ArrayList<Integer> bestSolution(String data){
+        ArrayList<Integer>solution = new ArrayList<>();
         String[] parts = data.split("\\p{Space}+");
+        if (parts.length < 3) return solution;
         int N = Integer.parseInt(parts[0]);
         int L = Integer.parseInt(parts[1]);
         ArrayList<Integer>days = new ArrayList<>();
@@ -12,7 +16,6 @@ public class P100{
         }
         int M = days.size();
         int[][] almacen = new int[N][M];
-        ArrayList<Integer>solution = new ArrayList<>();
         int pick=0, dont_pick=0;
         for (int i=0;i<N;i++) almacen[i][M-1] = days.get(M-1);
         for (int j=M-2;j>=0;j--){
@@ -25,36 +28,16 @@ public class P100{
         }
         int i=N-1, j=0;
         while (true){
-            if (almacen[i][j] == almacen[i][j+1]){
+            if (j+1 < M && almacen[i][j] == almacen[i][j+1]){
                 j = j+1;
             } else {
                 solution.add(j+1);
                 i = i-1; 
                 j = j+L;                
             }
-            if (i<0 || j==days.size()) break;
+            if (i<0 || j>=M) break;
         }
         return solution;
     }
 
-    public static void main(String[] args) {
-        P100 problem = new P100();
-        String data = "3 2 1 2 4 7 3";
-        ArrayList<Integer>solution = problem.bestSolution(data);
-        System.out.println("Data: " + data);
-        System.out.print("Solution: ");
-        for (int x : solution) {
-            System.out.print(x + " ");
-        }
-        System.out.print("\n\n\n");
-
-        data = "2 5 11 2 12 6 19 10 13 2 16 1";
-        solution = problem.bestSolution(data);
-        System.out.println("Data: " + data);
-        System.out.print("Solution: ");
-        for (int x : solution) {
-            System.out.print(x + " ");
-        }
-        System.out.print("\n\n\n");
-    }
 }
