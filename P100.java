94d3779e6b540b39c3db5f6d85204df0830685a5
Elsa Sarrías <elsa.sar.ali@gmail.com>
2018-11-11 23:04:23 +0100

    main solution

--- P100.java
+++ P100.java
@@ -1,109 +1,60 @@
 import java.util.ArrayList;
 
 public class P100{
-    int N;
-    int L; 
-    ArrayList<Integer>days;
-    int[][] almacen;
 
-    public int f(int i, int n){
-        // System.out.println("Call: i=" + i + " n=" + n);
-        if (n<0 || i>=days.size()) return 0;
-        else if (this.almacen[n][i] != -1) return this.almacen[n][i];
-        this.almacen[n][i] = Math.max(days.get(i) + f(i+L, n-1), f (i+1, n));
-        return this.almacen[n][i];
-    }
-
-    public Integer bestSolutionSimplePDRA(String data){
+    public ArrayList<Integer> bestSolution(String data){
         String[] parts = data.split("\\p{Space}+");
-        
-        N = Integer.parseInt(parts[0]);
-        L = Integer.parseInt(parts[1]);
-        days = new ArrayList<>();
+        int N = Integer.parseInt(parts[0]);
+        int L = Integer.parseInt(parts[1]);
+        ArrayList<Integer>days = new ArrayList<>();
         for (int i = 2; i<parts.length; i++){
             days.add(Integer.parseInt(parts[i]));
         }
-        this.almacen = new int[N][days.size()];
-        for (int i=0;i<N;i++){
-            for (int j=0;j<days.size();j++){
-                this.almacen[i][j] = -1;
-            }
-        }
-        return f(0, N-1);
-    }
-
-    public Integer bestSolutionSimplePDI(String data){
-        String[] parts = data.split("\\p{Space}+");
-        
-        N = Integer.parseInt(parts[0]);
-        L = Integer.parseInt(parts[1]);
-        days = new ArrayList<>();
-        for (int i = 2; i<parts.length; i++){
-            days.add(Integer.parseInt(parts[i]));
-        }
-        this.almacen = new int[N][days.size()];
+        int M = days.size();
+        int[][] almacen = new int[N][M];
+        ArrayList<Integer>solution = new ArrayList<>();
         int pick=0, dont_pick=0;
-        for (int i=0;i<N;i++) almacen[i][days.size()-1] = days.get(days.size()-1);
-        for (int j=days.size()-2;j>=0;j--){
+        for (int i=0;i<N;i++) almacen[i][M-1] = days.get(M-1);
+        for (int j=M-2;j>=0;j--){
             for (int i=N-1;i>=0;i--){
-                if (j+L >= days.size() || i==0) pick = days.get(j);
+                if (j+L >= M || i==0) pick = days.get(j);
                 else pick = days.get(j) + almacen[i-1][j+L];
                 dont_pick = almacen[i][j+1];
                 almacen[i][j] = Math.max(pick, dont_pick);
             }
         }
-        return almacen[N-1][0];
-    }
-
-    public int value(ArrayList<Integer>positions){
-        int v = 0;
-        for (Integer i : positions) {
-            v += days.get(i-1);
-        }
-        return v;
-    }
-
-    public ArrayList<Integer> f(ArrayList<Integer>positions, int i){ 
-        if (i>days.size() || positions.size()>=N) return positions; 
-        else {
-            ArrayList<Integer>dont_pick = new ArrayList<>(f(positions, i+1));
-            positions.add(i);
-            ArrayList<Integer>pick =  new ArrayList<>(f(positions, i+L));
-            int value_dont_pick = value(dont_pick);
-            int value_pick = -1; 
-            if (!pick.equals(positions)) value_pick = days.get(i-1) + value(pick);
-            if (value_pick >= value_dont_pick) return pick; 
-            else {
-                positions.remove(positions.size()-1);
-                return dont_pick;
+        int i=N-1, j=0;
+        while (true){
+            if (almacen[i][j] == almacen[i][j+1]){
+                j = j+1;
+            } else {
+                solution.add(j+1);
+                i = i-1; 
+                j = j+L;                
             }
+            if (i<0 || j==days.size()) break;
         }
+        return solution;
     }
-
-    // public ArrayList<Integer>bestSolution(String data){
-    //     String[] parts = data.split("\\p{Space}+");
-    //     N = Integer.parseInt(parts[0]);
-    //     L = Integer.parseInt(parts[1]);
-    //     ArrayList<Integer> days = new ArrayList<>();
-    //     for (int i = 2; i<parts.length; i++){
-    //         days.add(Integer.parseInt(parts[i]));
-    //     }
-    //     ArrayList<Integer>solution = new ArrayList<>();
-    //     return f(solution, 1);
-    // }
 
     public static void main(String[] args) {
         P100 problem = new P100();
-        int solution = problem.bestSolutionSimplePDI("3 2 1 2 4 7 3");
-        System.out.println("Solucion simple: " + solution);
+        String data = "3 2 1 2 4 7 3";
+        ArrayList<Integer>solution = problem.bestSolution(data);
+        System.out.println("Data: " + data);
+        System.out.print("Solution: ");
+        for (int x : solution) {
+            System.out.print(x + " ");
+        }
+        System.out.print("\n\n\n");
 
-        // ArrayList<Integer> complete_solution = problem.bestSolution("3 2 1 2 4 7 3");
-        // System.out.print("Solucion completa: ");
-        // for (Integer i : complete_solution) {
-        //     System.out.print(i + " ");
-        // }
-        
-        solution = problem.bestSolutionSimplePDI("2 5 11 2 12 6 19 10 13 2 16 1");
-        System.out.println("\nSolucion simple: " + solution);
+        data = "2 5 11 2 12 6 19 10 13 2 16 1";
+        solution = problem.bestSolution(data);
+        System.out.println("Data: " + data);
+        System.out.print("Solution: ");
+        for (int x : solution) {
+            System.out.print(x + " ");
+        }
+        System.out.print("\n\n\n");
     }
 }
